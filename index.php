581ad8aeba999fc7f9a59d8d7f22b44c7cfeac47
xgwshen <xgwshen@gmail.com>
2018-04-23 16:07:14 +0800

    添加函数html_entity_decode()

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
  * 测试系统函数
  */
 //adslashes 在预定义字符前加反斜杠
-$str="hhh\ null'sdfs',/hNULLh,NULL";
-echo addslashes($str);
-$str1=addslashes($str);
+//$str="hhh\ null'sdfs',/hNULLh,NULL";
+//echo addslashes($str);
+//$str1=addslashes($str);
 //stripslashes 把经过addslashes添加的反斜杠去掉
-echo stripslashes($str1);
+//echo stripslashes($str1);
 //htmlspecialchars 把预定义字符转化为html实体
-$str2="<a>baidu.com</a>";
-echo $str2;
-echo htmlspecialchars($str2);
+//$str2="<a>baidu.com</a>";
+//echo $str2;
+//echo htmlspecialchars($str2);
+////html_entity_decode()函数把HTML实体转化为字符 参数1:字符串 2:解码双引号与与单引号 3:字符集
+//$str="John &amp; &#039; Admin&#039; &gt";
+//echo html_entity_decode($str);
