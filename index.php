2860eea6b80ed0235efa233dd71efbaf943fffee
xgwshen <xgwshen@gmail.com>
2018-04-24 11:14:39 +0800

    strpos(string,find,start);current or false

--- index.php
+++ index.php
@@ -15,3 +15,6 @@
 ////html_entity_decode()函数把HTML实体转化为字符 参数1:字符串 2:解码双引号与与单引号 3:字符集
 //$str="John &amp; &#039; Admin&#039; &gt";
 //echo html_entity_decode($str);
+//strpos() 返回字符串在另一个字符串中第一次出现的位置 如果没有找到该字符串 返回false; strpos(string,find,start)
+//string find-必须 start开始搜索的位置 可选
+echo strpos("hello world",'wo');
